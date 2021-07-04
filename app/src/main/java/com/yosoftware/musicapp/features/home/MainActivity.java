33830416db4374a8fd16337e171f1b0bbc865cf7
JOE <joehamad97@gmail.com>
2021-07-04 12:21:54 +0300

    inital

---
+++ app/src/main/java/com/yosoftware/musicapp/features/home/MainActivity.java
@@ -0,0 +1,16 @@
+package com.yosoftware.musicapp.features.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.yosoftware.musicapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
